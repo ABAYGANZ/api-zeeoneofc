b41d8e01bec4840830e9df2cc775e3f157c0458f
ABAYGANZ <87226254+ABAYGANZ@users.noreply.github.com>
2021-07-11 15:00:35 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -16,9 +16,9 @@ router.get('/config', (req, res) => {
         status: true,
         result: {
             prefix : '/',
-            namabot: 'ZhirrrBot',
-            namaowner: 'Zhirrr',
-            instagram: 'zhirr_ajalah',
+            namabot: 'Zerobot',
+            namaowner: 'Abay',
+            instagram: 'sheyenk.gt',
             youtube : 'Gak Punya'
         }
     }
